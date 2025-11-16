22bccb0b9c0241a0b4a4b7d9b8b836a4154ecfa9
jayeshpal001 <jayashpal5@gmail.com>
2025-11-16 18:45:34 +0530

    axiosInstance added

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL, // your backend base URL
   withCredentials: true, //  send & receive cookies
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
