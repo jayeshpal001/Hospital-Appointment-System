448f3c4c83bdbf02d122167ea12b53405be34c2f
jayeshpal001 <jayashpal5@gmail.com>
2026-01-17 00:19:51 +0530

    fix axiosInstance

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -6,7 +6,7 @@ const api = axios.create({
 });
 
 // Request Interceptor: Har request ke sath Token jod dega
-axiosInstance.interceptors.request.use(
+api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
